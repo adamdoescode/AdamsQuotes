edb670396bd1a540f9439993fcd98f2c81f14ddc
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2022-12-12 13:49:44 +0800

    Added quote tags manually and now processing off of those

--- addTagsToRawQuotes.py
+++ addTagsToRawQuotes.py
@@ -4,10 +4,12 @@ Then I can manually go in and fix them up.
 
 I have a raw quotes file: sampleQuotesUnprocessed.md
 '''
-
+#%%
+from typing import List,Dict
 #%%
 
-with open("sampleQuotesUnprocessed.md", "r") as quotesFile:
+# with open("sampleQuotesUnprocessed.md", "r") as quotesFile:
+with open("testApple.md", "r") as quotesFile:
     quotes = quotesFile.read()
 
 '''
@@ -21,26 +23,96 @@ We can also identify links by the presence of 'http' in the line.
 Otherwise we leave things untouched.
 '''
 
-for quote in quotes.split('\n\n')[1:4]:
-    quote = quote.strip()
-    lines = quote.splitlines()
-    #if the quote comes from an iOS copy we treat it differently
-    if 'excerpt' in quote.lower():
-        #then it's a quote from iOS
-        pass
-    else:
-        #it is not a quote from iOS
-        for count,line in enumerate(lines):
-            line = line.strip()
-            if count == 0:
-                #assume quote main text
-                print(f'*quote:* {line}')
-            elif count == 1:
-                #assume source
-                print(f'*source:* {line}')
-            elif count == 2:
-                #assume note
-                print(f'*note:* {line}')
-            elif 'http' in line:
-                print(f'*link:* {line}')
+knownAuthors: List[str] = [
+    'tim low',
+    'david foster wallace',
+    'steve silberman',
+    'jennifer doudna',
+    'mark twain',
+    'Charles Darwin',
+    'Alastair Reynolds',
+    'Adam Rutherford',
+    'David Quammen'
+    'Sam Harris'
+]
+
+with open("sampleQuotesSemiProcessed.md", "w") as quotesFile:
+    for quote in quotes.split('*quote*')[1:]:
+        quote = quote.strip()
+        lines = quote.splitlines()
+        #remove empty rows
+        lines = [line for line in lines if len(line) > 0]
+        #values for each quote
+        quoteText: str = ''
+        sourceText: str = ''
+        noteText: str = ''
+        linkText: str = ''
+        authorText: str = ''
+        #if the quote comes from an iOS copy we treat it differently
+        if 'excerpt' in quote.lower():
+            #then it's a quote from iOS
+            #check for authors
+            for author in knownAuthors:
+                if author.lower() in quote.lower():
+                    authorText = author.title()
+            for count,line in enumerate(lines):
+                line = line.strip()
+                if count == 0:
+                    #assume quote main text
+                    quoteText = line
+                elif "Excerpt from".lower() in line.lower():
+                    #iterate to get to source line
+                    count,line = next(enumerate(lines))
+                    sourceText = line
+                if 'http' in line:
+                    linkText = line
+            if not quoteText == '':
+                quotesFile.write(f'*quote:*\t{quoteText}\n',)
+                quotesFile.write(f'*source:*\t{sourceText}\n',)
+                quotesFile.write(f'*author:*\t{authorText}\n',)
+                quotesFile.write(f'*link:*\t{linkText}\n',)
+                quotesFile.write(f'*note:*\t{noteText}\n\n',)
+                print(
+                    f'*quote:*\t{quoteText}\n',
+                    f'*source:*\t{sourceText}\n',
+                    f'*author:*\t{authorText}\n',
+                    f'*link:*\t{linkText}\n',
+                    f'*note:*\t{noteText}\n\n',
+                    sep=''
+                )
+                    
+        else:
+            #it is not a quote from iOS
+            for count,line in enumerate(lines):
+                if len(line) == 0:
+                    break
+                if count == 0:
+                    #assume quote main text
+                    quoteText = line
+                elif count == 1:
+                    #assume source
+                    sourceText = line
+                elif count == 2:
+                    #assume note
+                    noteText = line
+                elif 'http' in line:
+                    linkText = line
+                #check for authors
+                for author in knownAuthors:
+                    if author.lower() in line.lower():
+                        authorText = author.title()
+            if not quoteText == '':
+                quotesFile.write(f'*quote:*\t{quoteText}\n',)
+                quotesFile.write(f'*source:*\t{sourceText}\n',)
+                quotesFile.write(f'*author:*\t{authorText}\n',)
+                quotesFile.write(f'*link:*\t{linkText}\n',)
+                quotesFile.write(f'*note:*\t{noteText}\n\n',)
+                print(
+                    f'*quote:*\t{quoteText}\n',
+                    f'*source:*\t{sourceText}\n',
+                    f'*author:*\t{authorText}\n',
+                    f'*link:*\t{linkText}\n',
+                    f'*note:*\t{noteText}\n\n',
+                    sep=''
+                )
 # %%
---
+++ sampleQuotesSemiProcessed.md
@@ -0,0 +1,36 @@
+*quote:*	For 360 minutes per diem, we receive unconscious reinforcement of the deep thesis that the most significant quality of truly alive persons is watchableness, and that genuine human worth is not just identical with but rooted in the phenomenon of watching. 
+*source:*	A Supposedly Fun Thing I'll Never Do Again, David foster Wallace
+*author:*	David Foster Wallace
+*link:*	
+*note:*	
+
+*quote:*	Tasmania’s south-west, the great wilderness mecca, is by some criteria a biodiversity desert. Diversity of mammals, birds, reptiles, frogs, fish and plants is low. More reptile species (twelve) roam through my backyard than occupy south-west Tasmania, and my local creek, thick with weeds, supports many more kinds of rainforest tree, including an endangered myrtle.
+*source:*	The new nature Tim low
+*author:*	Tim Low
+*link:*	
+*note:*	
+
+*quote:*	Explorer Dirk Hartog summed matters up in 1616. ‘This land is cursed,’ he wrote, ‘the animals hop, not run, the birds run, not fly, and the swans are black, not white.’
+*source:*	The new nature Tim low
+*author:*	Tim Low
+*link:*	
+*note:*	
+
+*quote:*	We hear so much these days about wildlife dying out, as if nature en masse were sliding down the drain. The truth is more interesting. Many native things are thriving by riding on our coat-tails. Seagulls, ravens, magpies, red-back spiders and bulrushes have never known better times. Their futures brightened when people came to the great south land.
+*source:*	The new nature Tim low
+*author:*	Tim Low
+*link:*	
+*note:*	
+
+*quote:*	"Things look different without the Prophets' lies clouding my vision. I would like to see our own world - to know that it is safe." - To the Arbiter, on board of the Shadow of Intent.
+*source:*	
+*author:*	
+*link:*	
+*note:*	
+
+*quote:*	“The gesture that precipitates this insight for most people is an attempt to invert consciousness upon itself—to look for that which is looking—and to notice, in the first instant of looking for your self, what happens to the apparent divide between subject and object. Do you still feel that you are over there, behind your eyes, looking out at a world of objects?”
+*source:*	“The gesture that precipitates this insight for most people is an attempt to invert consciousness upon itself—to look for that which is looking—and to notice, in the first instant of looking for your self, what happens to the apparent divide between subject and object. Do you still feel that you are over there, behind your eyes, looking out at a world of objects?”
+*author:*	Sam Harris
+*link:*	
+*note:*	
+
