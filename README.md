6a76b081c6157c6a71fdcfb3327c1922eb8205b9
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2022-12-12 09:46:47 +0800

    Create a functional py script and html page

---
+++ README.md
@@ -0,0 +1,4 @@
+A simple project to pretty print my quotes collection to a HTML page.
+
+This is just practice for my python and html skills.
+
--- processQuotes.py
+++ processQuotes.py
@@ -1,36 +1,104 @@
+#%%
 '''
-A script to quickly process quotes from a markdown text file into a html file.
+A script to process quotes from a markdown text file into a html file.
 TODO Each quote will be contained within a div with a class of "quote".
 TODO div elements will be boxes (TODO flexbox) with a border and padding.
 TODO Each quote will have a class of "quote" and a unique id.
 '''
 
+#random int
+from random import randint
+from typing import List, Dict
+
 #%%
 
 with open("sampleQuotes.md", "r") as quotesFile:
-    quotes = quotesFile.readlines()
+    quotes = quotesFile.read()
 
-# with open("quotes.html", "w") as htmlFile:
-#     for quote in quotes:
-#         htmlFile.write(f"
+class Quote:
+    '''
+    A class to hold simple quote information
+    Does not include HTML features!
+    '''
+    def __init__(self):
+        self.quote: str = ''
+        self.source: str = ''
+        self.note: str = ''
+        '''
+        Very simple random generator for now, 
+        maybe replace with something more meaningful if that turns out to be useful
+        '''
+        self.id = randint(10000,99999)
 
 class ProcessQuotes:
-
-    def __init__(self, quotes):
-        self.quotes = quotes
-    
-    def FindTags(self):
-        for quote in self.quotes:
-            if quote.startswith("#"):
-                print(quote)
+    '''
+    Processes quotes from markdown file into html divs.
+    '''
+    def __init__(self, quotes: str):
+        self.linesFromFile = quotes #raw quotes List with string items
+        #these will be used for div class names
+        self.tags = {
+            '*Quote*': 'quote',
+            '*Source:*': 'source',
+            '*Note:*': 'note',
+        }
+        #a list to hold our quotes in as a Quote class
+        self.QuotesList: List[Quote] = []
 
     def processQuotes(self):
-        for quote in self.quotes:
-            print(quote)
+        '''
+        Coordinating function that cycles through quote lines
+        First we split by the quote tag so that:
+            1. the quote is the first line
+            2. the source and note are within the same block of text
+            (but on seperate lines somewhere)
+        '''
+        for RawQuote in self.linesFromFile.split('*Quote*')[1:]:
+            linesIterator = iter(RawQuote.splitlines())
+            newQuote = Quote()
+            for line in linesIterator:
+                line = line.strip()
+                if '*Source:*' in line:
+                    newQuote.source = line.replace('*Source:* ','')
+                elif '*Note:*' in line:
+                    newQuote.note = line.replace('*Note:* ','')
+                else:
+                    newQuote.quote += line
+            self.QuotesList.append(newQuote)
+        return self
+
+    def printQuotes(self, numQuotes: int = None):
+        counter = 0
+        for quote in self.QuotesList:
+            if counter == numQuotes:
+                break
+            print(f'quote: {quote.quote}')
+            print(f'source: {quote.source}')
+            print(f'note: {quote.note}')
+            print(f'id: {quote.id}')
+            print()
+            counter += 1
 
     def writeQuotes(self):
-        with open("quotes.html", "w") as htmlFile:
-            for quote in self.quotes:
-                htmlFile.write(f"")
+        with open('index.html', 'w') as quotesFile:
+            for quote in self.QuotesList:
+                quotesFile.write('''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>''')
+                quotesFile.write(f'<div class="quote" id="{quote.id}">\n')
+                quotesFile.write(f'  <p class="note">{quote.note}</p>\n')
+                quotesFile.write(f'  <p class="quote-text">{quote.quote}</p>\n')
+                quotesFile.write(f'  <p class="source">{quote.source}</p>\n')
+                quotesFile.write('</div>\n')
+                quotesFile.write('</body>\n</html>')
 
+processedQuotes = ProcessQuotes(quotes).processQuotes()
+# processedQuotes.printQuotes(2)
+processedQuotes.writeQuotes()
 # %%
