5600c3b173b0de748ee6f960192dcf59b76ce0cf
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-02-19 14:37:25 +0800

    remove index from table of contents

--- processQuotes.py
+++ processQuotes.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 '''
 A script to process quotes from a markdown text file into a html file.
 
@@ -13,32 +13,32 @@ TODO div elements will be boxes (TODO flexbox) with a border and padding.
 TODO Each quote will have a class of "quote" and a unique id.
 '''
 
-#random int
+# random int
 from random import randint
 from typing import List, Dict
 import pandas as pd
 
-#%%
 
 class Quote:
     '''
     A class to hold simple quote information
     Does not include HTML features!
     '''
+
     def __init__(self):
-        self.quote: str = '' #always present
+        self.quote: str = ''  # always present
         self.source: str = ''
         self.note: str = ''
-        self.link: str = '' #sometimes present
-        self.author: str = '' #sometimes present
+        self.link: str = ''  # sometimes present
+        self.author: str = ''  # sometimes present
         '''
         Very simple random generator for now, 
         maybe replace with something more meaningful if that turns out to be useful
         Maybe for alternating div colours.
         '''
-        self.idForQuote = randint(10000,99999)
+        self.idForQuote = randint(10000, 99999)
         self.title: str = ''
-    
+
     def generateTitle(self):
         '''
         A function to generate a title for the quote.
@@ -46,27 +46,28 @@ class Quote:
         1. if the quote is less than 8 words, use the whole quote.
         2. if the quote is more than 8 words, use the first 8 words and add an ellipsis.
         '''
-        #split the quote into a list of words
+        # split the quote into a list of words
         quoteWords = self.quote.split()
-        #if the quote is less than 8 words, use the whole quote
-        #deal with empty quotes too
+        # if the quote is less than 8 words, use the whole quote
+        # deal with empty quotes too
         if len(quoteWords) == 0:
             self.title = ''
         elif len(quoteWords) < 8:
             self.title = self.quote
-        #if the quote is more than 8 words, use the first 8 words and add an ellipsis
+        # if the quote is more than 8 words, use the first 8 words and add an ellipsis
         elif len(quoteWords) > 8:
             self.title = ' '.join(quoteWords[:8]) + '...'
         return self
-        
+
 
 class ProcessQuotes:
     '''
     Processes quotes from markdown file into html divs.
     '''
+
     def __init__(self, quotes: str):
-        self.linesFromFile = quotes #raw quotes List with string items
-        #a list to hold our quotes in as a Quote class
+        self.linesFromFile = quotes  # raw quotes List with string items
+        # a list to hold our quotes in as a Quote class
         self.QuotesList: List[Quote] = []
         '''
         For a table of contents we need a way to reference divs by title.
@@ -89,23 +90,28 @@ class ProcessQuotes:
         Before we write the quotes to the html file we need to write the table of contents.
         We can leverage pandas to print a table to html for this.
         '''
-        #start the table of contents
+        # start the table of contents
         tableOfContents = '<div class="table-of-contents">\n'
-        tableOfContentsDictForPandas = {column:[] for column in ['title','source']}
-        #itertate through the quotes in the QuoteTitles dict
+        tableOfContentsDictForPandas = {column: []
+                                        for column in ['title', 'source']}
+        # itertate through the quotes in the QuoteTitles dict
         for quote in self.QuotesList:
-            #add the index, title, and blank columns to the table of contents dict
+            # add the index, title, and blank columns to the table of contents dict
             titleLink = f'  <p class="tableOfContents"><a class="tableOfContents" href="#{quote.idForQuote}">{quote.title}</a></p>'
             tableOfContentsDictForPandas['title'].append(titleLink)
-            #truncate source length in title
+            # truncate source length in title
             if len(quote.source) > 30:
-                tableOfContentsDictForPandas['source'].append(quote.source[:30] + '...')
+                tableOfContentsDictForPandas['source'].append(
+                    quote.source[:30] + '...')
             else:
                 tableOfContentsDictForPandas['source'].append(quote.source)
-        tableOfContents += pd.DataFrame(tableOfContentsDictForPandas).to_html(header=True, justify='left', border=0, render_links=True)
-        #need to fix a unicode translation issue which I assume is a safety thing
-        tableOfContents = tableOfContents.replace('&lt;','<').replace('&gt;','>')
-        #make sure to include a closing div tag
+        tableOfContents += pd.DataFrame(tableOfContentsDictForPandas).to_html(
+            header=True, justify='left', border=0, render_links=True, index=False
+        )
+        # need to fix a unicode translation issue which I assume is a safety thing
+        tableOfContents = tableOfContents.replace(
+            '&lt;', '<').replace('&gt;', '>')
+        # make sure to include a closing div tag
         return tableOfContents + '</div>\n'
 
     def processQuotes(self):
@@ -128,13 +134,16 @@ class ProcessQuotes:
             # linesIterator = iter(RawQuote.splitlines())
             newQuote = Quote()
             newQuote.quote = RawQuote.split('*source:*')[0].strip()
-            newQuote.source = RawQuote.split('*source:*')[1].split('*author:*')[0].strip()
-            newQuote.author = RawQuote.split('*author:*')[1].split('*link:*')[0].strip()
-            newQuote.link = RawQuote.split('*link:*')[1].split('*note:*')[0].strip()
+            newQuote.source = RawQuote.split(
+                '*source:*')[1].split('*author:*')[0].strip()
+            newQuote.author = RawQuote.split(
+                '*author:*')[1].split('*link:*')[0].strip()
+            newQuote.link = RawQuote.split(
+                '*link:*')[1].split('*note:*')[0].strip()
             newQuote.note = RawQuote.split('*note:*')[1].strip()
-            #generate a title for the quote
+            # generate a title for the quote
             newQuote.generateTitle()
-            #then we add the quote to the QuoteTitles dict for use in a table of contents
+            # then we add the quote to the QuoteTitles dict for use in a table of contents
             self.createQuoteTitles(newQuote)
             self.QuotesList.append(newQuote)
         return self
@@ -162,44 +171,44 @@ class ProcessQuotes:
         if quote.title != '':
             quoteInHTML += f'  <p class="quote-title">{quote.title}</p>\n'
         if quote.note != '':
-            #iterate through each paragraph and pass as <p> tag
+            # iterate through each paragraph and pass as <p> tag
             for note in quote.note.splitlines():
                 quoteInHTML += f'  <p class="note">{note}</p>\n'
-        #do the same for the potentatially multiline quote
+        # do the same for the potentatially multiline quote
         for quoteLine in quote.quote.splitlines():
             quoteInHTML += f'  <p class="quote-text">{quoteLine}</p>\n'
-        #source
+        # source
         if quote.source != '':
             quoteInHTML += f'  <p class="source"><span class="source-source">Source:</span> {quote.source}</p>\n'
-        #author
+        # author
         if quote.author != '':
             quoteInHTML += f'  <p class="author"><span class="tag-author">Author:</span> {quote.author}</p>\n'
-        #link
+        # link
         if quote.link != '':
             quoteInHTML += f'  <p class="link"><span class="tag-link">Link:</span> <a class="tag-link-href" href="{quote.link}">{quote.link}<a></p>\n'
         quoteInHTML += '</div>\n'
         return quoteInHTML
-    
+
     def writeQuotes(self):
         # get headerAndFooter scaffold
         with open('Header.html', 'r') as headerAndFooterFile:
             headerAndFooter = headerAndFooterFile.read()
         quotesHtmlFormatted = ''
-        #first write the table of contents
+        # first write the table of contents
         quotesHtmlFormatted += self.writeTableOfContents()
-        #now we generate a string of the quotes with divs etc
+        # now we generate a string of the quotes with divs etc
         for quote in self.QuotesList:
             quotesHtmlFormatted += self.writeQuoteAttributeToFile(quote)
-        headerAndFooter = headerAndFooter.replace('<!--quotes-->', quotesHtmlFormatted)
+        headerAndFooter = headerAndFooter.replace(
+            '<!--quotes-->', quotesHtmlFormatted)
         with open('index.html', 'w') as quotesFile:
             quotesFile.write(headerAndFooter)
 
-with open("sampleQuotesProcessed.md", "r") as quotesFile:
-    quotes = quotesFile.read()
 
-processedQuotes = ProcessQuotes(quotes).processQuotes()
-processedQuotes.printQuotes(2)
-processedQuotes.writeQuotes()
-# %%
-processedQuotes.QuoteTitles
+if __name__ == "__main__":
+    with open("sampleQuotesProcessed.md", "r") as quotesFile:
+        quotes = quotesFile.read()
+    processedQuotes = ProcessQuotes(quotes).processQuotes()
+    processedQuotes.writeQuotes()
+
 # %%
